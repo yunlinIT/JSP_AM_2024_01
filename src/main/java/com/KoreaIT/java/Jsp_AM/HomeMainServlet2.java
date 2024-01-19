622d2d3f0182d64cf28439704e9d5b0ebf808597
yunlinIT <yunlinit@gmail.com>
2024-01-19 16:41:01 +0900

    구구단 기초 (수정)

--- src/main/java/com/KoreaIT/java/Jsp_AM/HomeMainServlet2.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/HomeMainServlet2.java
@@ -7,23 +7,18 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/printDan")
+@WebServlet("/home/main2")
 public class HomeMainServlet2 extends HttpServlet {
 	
   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-		response.setContentType("text/html;charset=UTF-8");
-		response.getWriter().append("==8단==<br>");
-		
-		int dan = 8;
-		for (int i = 1; i <= 9; i++) {
-			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
-		
 
-		}
+		response.getWriter().append("Hello World!!!!!!!!!!");
+		
+		
+		
 	}
-	
 	
 	
 }
---
+++ src/main/java/com/KoreaIT/java/Jsp_AM/printDanServlet.java
@@ -0,0 +1,28 @@
+package com.KoreaIT.java.Jsp_AM;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/printDan")
+public class printDanServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html;charset=UTF-8");
+
+		response.getWriter().append("==8단==<br>");
+
+		int dan = 8;
+
+		for (int i = 1; i <= 9; i++) {
+			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
+		}
+	}
+
+}

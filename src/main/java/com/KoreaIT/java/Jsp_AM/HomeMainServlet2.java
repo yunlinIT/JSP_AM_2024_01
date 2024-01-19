78f5df4bc54053061dc8e2027689f54bb987ce26
yunlinIT <yunlinit@gmail.com>
2024-01-19 15:49:22 +0900

    서블릿 생성

---
+++ src/main/java/com/KoreaIT/java/Jsp_AM/HomeMainServlet2.java
@@ -0,0 +1,20 @@
+package com.KoreaIT.java.Jsp_AM;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/home/main2")
+public class HomeMainServlet2 extends HttpServlet {
+	
+  
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		response.getWriter().append("Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!").append(request.getContextPath());
+	}
+	
+
+}

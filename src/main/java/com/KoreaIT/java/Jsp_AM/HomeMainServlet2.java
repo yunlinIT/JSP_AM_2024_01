f1f26516ebf84daed587a441a81d1c59e3d2ab94
yunlinIT <yunlinit@gmail.com>
2024-01-19 16:32:22 +0900

    구구단 기초

--- src/main/java/com/KoreaIT/java/Jsp_AM/HomeMainServlet2.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/HomeMainServlet2.java
@@ -7,14 +7,23 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/home/main2")
+@WebServlet("/printDan")
 public class HomeMainServlet2 extends HttpServlet {
 	
   
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
-		response.getWriter().append("Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF-8");
+		response.getWriter().append("==8단==<br>");
+		
+		int dan = 8;
+		for (int i = 1; i <= 9; i++) {
+			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan * i));
+		
+
+		}
 	}
 	
-
+	
+	
 }

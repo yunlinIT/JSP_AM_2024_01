5f3495b1d9ae84c6a85dd9a128171e6269ee651c
yunlinIT <yunlinit@gmail.com>
2024-01-22 10:09:56 +0900

    서블릿이 JSP 파일에게 요청정보를 넘겨줘서 협업이 가능하도록

--- src/main/java/com/KoreaIT/java/Jsp_AM/HomePrintDanServlet.java
+++ src/main/java/com/KoreaIT/java/Jsp_AM/HomePrintDanServlet.java
@@ -7,18 +7,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/home/main2")
-public class HomeMainServlet2 extends HttpServlet {
-	
-  
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-			throws ServletException, IOException {
+@WebServlet("/home/printDan")
+public class HomePrintDanServlet extends HttpServlet {
 
-		response.getWriter().append("Hello World!!!!!!!!!!");
-		
-		
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		request.getRequestDispatcher("/jsp/home/printDan.jsp").forward(request, response);
+
 	}
-	
-	
+
 }
---
+++ src/main/webapp/jsp/home/printDan.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+	pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>구구단 출력</title>
+</head>
+<body>
+	<h1>==8단==</h1>
+	<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+		Maxime natus doloremque distinctio fugiat nihil a architecto veritatis
+		aperiam esse quod quas ipsam id quo beatae tempora illum tenetur in
+		deserunt?</div>
+</body>
+</html>
